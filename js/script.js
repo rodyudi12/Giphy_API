588e06599ac39bb0a3c8b06f1cedeb8b12953b16
rodyudi12 <rodrigo.hirashiki@uhsp.edu>
2026-03-25 16:29:28 -0500

    Getting your API endpoint

--- js/script.js
+++ js/script.js
@@ -1,1 +1,3 @@
 console.log("script.js loaded");
+
+let endpoint = "https://api.giphy.com/v1/gifs/search?api_key=VBSOnqSX8c0Gd09ipgGZlNbiD1QeZTDu&q=dogs&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips"
