58068554633ca5babdeacac58177c4b02930ac40
rodyudi12 <rodrigo.hirashiki@uhsp.edu>
2026-03-25 16:40:16 -0500

    fetch gifs

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
 console.log("script.js loaded");
 
-let endpoint = "https://api.giphy.com/v1/gifs/search?api_key=VBSOnqSX8c0Gd09ipgGZlNbiD1QeZTDu&q=dogs&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips"
+ const endpoint = "https://api.giphy.com/v1/gifs/search?api_key=VBSOnqSX8c0Gd09ipgGZlNbiD1QeZTDu&q=dogs&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips"
+
+ async function fetchGifs() {
+    let response = await fetch(endpoint);
+    let data = await response.json();
+
+    let images = data.data.map(gif => gif.images.original.url);
+
+    console.log(images);
+ }
+
+fetchGifs();
