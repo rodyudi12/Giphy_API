f4b7717cbaa6df4a64701828a3cfe779007eda9c
rodyudi12 <rodrigo.hirashiki@uhsp.edu>
2026-03-27 15:33:30 -0500

    Display GIFs on the Page

--- js/script.js
+++ js/script.js
@@ -2,13 +2,22 @@ console.log("script.js loaded");
 
  const endpoint = "https://api.giphy.com/v1/gifs/search?api_key=VBSOnqSX8c0Gd09ipgGZlNbiD1QeZTDu&q=dogs&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips"
 
- async function fetchGifs() {
+ const gifContainer = document.querySelector("#gif-container");
+ const button = document.querySelector("#fetch-gif-btn");
+
+ button.addEventListener("click", async function () {
+   
+   gifContainer.innerHTML = "";
+
     let response = await fetch(endpoint);
     let data = await response.json();
 
     let images = data.data.map(gif => gif.images.original.url);
 
     console.log(images);
- }
 
-fetchGifs();
+ for (let i = 0; i < images.length; i++) {
+   gifContainer.innerHTML += `<img src=${images[i]} class = "col-3 mb-3">`;
+ }
+});
+
