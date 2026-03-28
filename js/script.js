63d07cad09117d9481e5d78063e450c69041d883
rodyudi12 <rodrigo.hirashiki@uhsp.edu>
2026-03-28 11:07:52 -0500

    Debugged final output issues

--- js/script.js
+++ js/script.js
@@ -1,18 +1,20 @@
 console.log("script.js loaded");
 
- const endpoint = "https://api.giphy.com/v1/gifs/search?api_key=VBSOnqSX8c0Gd09ipgGZlNbiD1QeZTDu&q=dogs&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips"
 
  const gifContainer = document.querySelector("#gif-container");
  const button = document.querySelector("#fetch-gif-btn");
+ const searchInput = document.querySelector("#search-input");
 
  button.addEventListener("click", async function () {
    
    gifContainer.innerHTML = "";
+   
+   let searchTerm = searchInput.value.trim();
+   const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=VBSOnqSX8c0Gd09ipgGZlNbiD1QeZTDu&q=${searchTerm}&limit=25`;
+   let response = await fetch(endpoint);
+   let data = await response.json();
 
-    let response = await fetch(endpoint);
-    let data = await response.json();
-
-    let images = data.data.map(gif => gif.images.original.url);
+   let images = data.data.map(gif => gif.images.original.url);
 
     console.log(images);
 
